29d3b3405c186e61cff7a7f2561032e2c29ea547
jkprerana <33819336+jkprerana@users.noreply.github.com>
2019-03-22 11:07:47 +0530

    newVersions

--- ConstructST.py
+++ ConstructST.py
@@ -1,12 +1,14 @@
 import plyLex
 import sys
+# import math
 
 # if len(sys.argv) < 2:
 #     print("Please Enter the name of the Source file")
 #     exit(0)
-data = open("ex2.r","r")
 
-data = data.read()
+
+f = open(sys.argv[1])
+data = f.read()
 tokens = plyLex.generateToken(data)
 
 symbolTable = dict()
@@ -16,18 +18,27 @@ stack.append(currentTable)
 entry = 1
 
 for t in tokens:
-    if t.value != '{' and t.value != '}':
-        if t.value not in stack[-1]:
-            stack[-1][t.value] = [t.type,t.lineno]
-    elif t.value == '{':
-        currentTable = stack[-1]['scope'+str(entry)] = dict()   #using to the advantage of shallow copy....i.e we create a reference
-        stack.append(currentTable)
-        entry+=1
+    # print(t)
+    # print(type(t.value))
+    # print(symbolTable)
+    # print(currentTable)
+    # print(t.value)
+    if (t.type != "ID"):
+        continue
+    else:
+    	if t.value != '{' and t.value != '}':
+        	if t.value not in stack[-1]:
+        		stack[-1][t.value] = [t.type, t.lineno, -1, -1]
+    	elif t.value == '{':
+    	    currentTable = stack[-1]['scope'+str(entry)] = dict()   #using to the advantage of shallow copy....i.e we create a reference
+    	    stack.append(currentTable)
+    	    entry+=1
 
-    elif t.value ==  '}':
-        stack.pop()
-        if len(stack) == 0:
-            stack.append(symbolTable)
+    	elif t.value ==  '}':
+        	stack.pop()
+        	if len(stack) == 0:
+        		stack.append(symbolTable)
 
+print("NAME TOKEN LINE NO. INITIAL VALUE TYPE OF ATTRIBUTE")
 for k,v in symbolTable.items():
     print(k,"  ",v)
---
+++ correctIP.r
@@ -0,0 +1,15 @@
+a = 4 + 5 -3 * 6
+
+n <- 10
+
+for (i in (0:n))
+{
+	print(i)
+}
+
+n <- 11
+for (i in c(0:n))
+{
+	print(i)
+}
+
